5df25661f852bcb7524b2f2ace9d2fc2cba87454
Andrei Shved <svensk.tut.by@gmail.com>
2022-01-19 21:45:35 +0300

    feat: add movies request

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,36 @@
 import React, { FC } from 'react';
+import { gql, useQuery } from '@apollo/client';
 
-const Home: FC = () => <>Main page</>;
+type Movie = {
+    id: number;
+    title: string;
+    medium_cover_image: string;
+};
+
+const GET_MOVIES = gql`
+    query {
+        movies {
+            id
+            title
+            medium_cover_image
+        }
+    }
+`;
+
+const Home: FC = () => {
+    const { loading, error, data } = useQuery(GET_MOVIES);
+
+    if (loading) return <>Loading...</>;
+
+    return data?.movies ? (
+        <ul>
+            {data?.movies.map((movie: Movie, idx: number) => (
+                <li key={idx}>{movie?.title}</li>
+            ))}
+        </ul>
+    ) : (
+        <div>No any movies</div>
+    );
+};
 
 export default Home;
