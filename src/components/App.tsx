8b92abd91f9f933227e937085c396ced679d4218
Andrei Shved <svensk.tut.by@gmail.com>
2022-01-19 21:05:56 +0300

    feat: add necessary dependencies

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,18 @@
 import React, { FC } from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from '../routes/Home';
+import Details from '../routes/Details';
 
 const App: FC = () => {
-    return <div className="app">App</div>;
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/:id" element={<Details />} />
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
---
+++ src/routes/Details.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from 'react';
+
+const Details: FC = () => <>About the movie</>;
+
+export default Details;
