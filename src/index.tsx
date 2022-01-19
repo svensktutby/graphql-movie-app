fc1f7310f04ddd0eccb36f85a06648af949ab821
Andrei Shved <svensk.tut.by@gmail.com>
2022-01-19 21:20:36 +0300

    feat: add apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from '@apollo/client';
 
+import client from './apollo';
 import App from './components/App';
 
 import './index.css';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
     </React.StrictMode>,
     document.getElementById('root'),
 );
